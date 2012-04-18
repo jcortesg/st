3f94335956483b2e7e04c59409fd7b7140a416e5
Rodrigo Dominguez <rorra@rorra.com.ar>
2012-04-18 16:30:24 -0300

    Admin can approbe/disaprobe accounts

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,20 @@ class Admin::UsersController < ApplicationController
   def index
     @users = User.all_except_admin
   end
+
+  def approve
+    @user = User.find(params[:id])
+    @user.approve unless @user.approved
+
+    flash[:notice] = "El usuario ha sido activado"
+    redirect_to [:admin, :users]
+  end
+
+  def disapprove
+    @user = User.find(params[:id])
+    @user.update_attribute(:approved, false) if @user.approved
+
+    redirect_to [:admin, :users]
+  end
+
 end
