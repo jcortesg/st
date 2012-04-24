4dcb72a7225ea643c94d4fc29a1766fec60bf994
Rodrigo Dominguez <rorra@rorra.com.ar>
2012-04-24 16:49:36 -0300

    Fixing privacy link

--- config/routes.rb
+++ config/routes.rb
@@ -57,7 +57,7 @@ Borwin::Application.routes.draw do
   match "proces-contact" => "home#process_contact", :as => :process_contact
 
   match "terms" => "home#terms", :as => :terms
-  match "terms" => "home#privacy", :as => :privacy
+  match "privacy" => "home#privacy", :as => :privacy
 
   root :to => 'home#index'
 
