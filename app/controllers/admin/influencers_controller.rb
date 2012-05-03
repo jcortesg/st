abe61eb67cc62f16931f5c748ae0f67248843648
Rodrigo Dominguez <rorra@rorra.com.ar>
2012-05-03 10:46:06 -0300

    Updating admin for the website

--- app/controllers/admin/influencers_controller.rb
+++ app/controllers/admin/influencers_controller.rb
@@ -4,6 +4,7 @@ class Admin::InfluencersController < ApplicationController
 
   # Show the list of influencers
   def index
-
+    @search = Influencer.includes(:user).search(params[:search])
+    @influencers = @search.page(params[:page])
   end
 end
--- app/helpers/admin/dashboard_helper.rb
+++ app/helpers/admin/dashboard_helper.rb
@@ -1,2 +1,9 @@
 module Admin::DashboardHelper
+  def img_link_to_twitter(influencer)
+    if influencer.twitter_image_url.size > 0
+      link_to(image_tag(influencer.twitter_image_url), "https://twitter.com/#!/#{influencer.user.twitter_screen_name}", target: '_blank')
+    else
+      "&nbsp;"
+    end
+  end
 end
