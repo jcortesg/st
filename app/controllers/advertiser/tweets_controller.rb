545a07960638d15dc7e7b583fc64ec0173aaf90a
Rodrigo Dominguez <rorra@rorra.com.ar>
2012-06-04 16:40:43 -0300

    Influencer list when creating a campaign

--- app/controllers/advertiser/tweets_controller.rb
+++ app/controllers/advertiser/tweets_controller.rb
@@ -1,2 +1,28 @@
 class Advertiser::TweetsController < ApplicationController
+  # Shows the list of celebrities (based on the campaigns) to create a new tweet
+  def index
+    @campaign = Campaign.find(params[:campaign_id])
+    @influencers = Influencer.page(params[:page]).per(50)
+  end
+
+  #
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 end
