9d62f646a4506565ffd44f2d1bfd70a1b887210d
Rodrigo Dominguez <rorra@rorra.com.ar>
2012-06-04 17:07:50 -0300

    UPdating twitter config

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,7 @@
+Twitter.configure do |config|
+  config.consumer_key = 'SF8RlPZT7bUW5QI173sJdg'
+  config.consumer_secret = 'JopN0OyZ7QQVpkO1DUEkQ5g8q5RlF17kHfEsU9NKo'
+  config.oauth_token = '144391600-S1sNJxqZI3NaO5zC1NtKGOGi2uWyr05vP9yjAt9k'
+  config.oauth_token_secret = '4PYb6smt0R9Izj2oFly1w9kHOBbIkpqj9mvV1KkUAPw'
+end
+
