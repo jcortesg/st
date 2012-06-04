642770c577976178245e647ca2199c43292462d5
Rodrigo Dominguez <rorra@rorra.com.ar>
2012-06-04 04:25:52 -0300

    Integrating new fields for audiences and campaigns

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -5,17 +5,14 @@ class Campaign < ActiveRecord::Base
 
   serialize :locations, Array
 
-  attr_accessible :name, :min_followers, :max_followers, :min_males, :max_males, :min_females, :max_females,
-                  :min_kids, :max_kids, :min_adults, :max_adults, :min_elderly, :max_elderly,
-                  :min_young_teens, :max_young_teens, :min_mature_teens, :max_mature_teens,
-                  :min_young_adults, :max_young_adults, :min_mature_adults, :max_mature_adults,
-                  :min_sports, :max_sports, :min_fashion, :max_fashion, :min_music, :max_music,
-                  :min_movies, :max_movies, :min_politics, :max_politics
+  attr_accessible :name, :objective, :locations, :males, :females, :moms, :teens, :college_students,
+                  :young_women, :young_men, :adult_women, :adult_men, :sports, :fashion, :music,
+                  :movies, :politics, :followers_quantity
 
   validates :name, uniqueness: { scope: :advertiser_id }
-  validates :objectives, presence: true
+  validates :objective, presence: true
 
-  validates :followers_quantity, presence: true, on: [:update]
+  validates :followers_quantity, presence: true, on: :update
 
   class << self
     # Brings the archived campaigns
