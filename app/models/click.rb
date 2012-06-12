1084da2d9446b470b120f587abfaccb95210e627
Rodrigo Dominguez <rorra@rorra.com.ar>
2012-06-12 06:34:19 -0300

    When a click is made, the campaign counter is updated

--- app/models/click.rb
+++ app/models/click.rb
@@ -1,5 +1,14 @@
 class Click < ActiveRecord::Base
   belongs_to :tweet
 
+  after_create :update_campaign_tweets
+
   attr_accessible :tweet, :tweet_id, :remote_ip, :remote_agent
+
+  private
+
+  # Updates the campaign counter
+  def update_campaign_tweets
+    ActiveRecord::Base.connection.execute("UPDATE campaigns SET clicks_count = clicks_count + 1 WHERE id = #{tweet.campaign_id}")
+  end
 end
