a089c7a956fafffcf79c28681032cbe682047917
Rodrigo Dominguez <rorra@rorra.com.ar>
2012-06-12 09:59:33 -0300

    Clicks for campaign/tweet

--- app/controllers/advertiser/clicks_controller.rb
+++ app/controllers/advertiser/clicks_controller.rb
@@ -1,18 +1,23 @@
 # encoding: utf-8
 class Advertiser::ClicksController < ApplicationController
-  before_filter :find_campaign
+  before_filter :find_campaign_and_tweet
 
   def index
     search_params = params[:search] || {}
     search_params.reverse_merge!({"meta_sort" => "created_at.desc"})
 
-    @search = Click.where("tweet_id in (?)", @campaign.tweets.all.collect { |t| t.id }).search(params[:search])
+    if @tweet
+      @search = Click.where(tweet_id: @tweet.id).search(params[:search])
+    else
+      @search = Click.where("tweet_id in (?)", @campaign.tweets.all.collect { |t| t.id }).search(params[:search])
+    end
     @clicks = @search.page(params[:page])
   end
 
   private
 
-  def find_campaign
+  def find_campaign_and_tweet
     @campaign = current_role.campaigns.find(params[:campaign_id])
+    @tweet = @campaign.tweets.find(params[:tweet_id]) if params[:tweet_id]
   end
 end
