2ec8837a29571e5670e6aaac0b91b37dbe747cc7
Rodrigo Dominguez <rorra@rorra.com.ar>
2012-06-22 01:44:21 -0300

    Keywords for audiences

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -2,4 +2,10 @@ class Keyword < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
 
   attr_accessible :name, :keywords
+
+  before_save :normalize_keywords
+
+  def normalize_keywords
+    self.keywords = self.keywords.gsub(/ +,/, ',').gsub(/, +/, ',').gsub(/ +/, ' ').gsub("\n", "").gsub("-", ",").downcase
+  end
 end
