0be4d405e073d74acb80e418d758d8478b711dd2
Rodrigo Dominguez <rorra@rorra.com.ar>
2012-06-22 21:42:50 -0300

    Transactions done

--- app/helpers/admin/transactions_helper.rb
+++ app/helpers/admin/transactions_helper.rb
@@ -1,2 +1,8 @@
 module Admin::TransactionsHelper
+  def admin_attachable_link(attachable)
+    return '&nbsp'.html_safe if attachable.nil?
+    if attachable.class.to_s == "Tweet"
+      link_to("Tweet", [:advertiser, attachable.campaign])
+    end
+  end
 end
