bada92d7a3ff62cf1a61562670ce6d503444c49d
Rodrigo Dominguez <rorra@rorra.com.ar>
2012-07-11 06:52:13 -0300

    After a tweet is accepted, the hashtags are created. When a campaign has hashtags, they are shown

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -1,5 +1,7 @@
 class Hashtag < ActiveRecord::Base
   belongs_to :campaign
 
+  attr_accessible :campaign, :campaign_id, :hashtag
+
   validates :hashtag, uniqueness: { scope: :campaign_id }, presence: true
 end
