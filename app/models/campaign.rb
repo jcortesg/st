393e80b53c4e6a45069fcb9d49315de0ceee4b32
Rodrigo Dominguez <rorra@rorra.com.ar>
2012-07-11 06:17:45 -0300

    New UI for creating campaigns

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 class Campaign < ActiveRecord::Base
   belongs_to :advertiser
   has_many :tweets, dependent: :destroy
@@ -9,12 +10,13 @@ class Campaign < ActiveRecord::Base
   serialize :followers_qty, Array
   serialize :tweet_price, Array
 
-  attr_accessible :name, :objective, :locations, :males, :females, :moms, :teens, :college_students,
-                  :young_women, :young_men, :adult_women, :adult_men, :sports, :fashion, :music,
+  attr_accessible :name, :objective, :twitter_screen_name, :locations, :males, :females, :moms, :teens,
+                  :college_students, :young_women, :young_men, :adult_women, :adult_men, :sports, :fashion, :music,
                   :movies, :politics, :technology, :travel, :luxury, :followers_qty, :tweet_price, :price_per_click
 
   validates :name, uniqueness: { scope: :advertiser_id }
   validates :objective, presence: true
+  validate :twitter_screen_name_validation
 
   validates :followers_qty, presence: true, on: :update
 
@@ -53,6 +55,23 @@ class Campaign < ActiveRecord::Base
     # Brings the active campaigns
     def active
       where(statuc: 'active')
+    end
+  end
+
+  private
+
+  # Check the twitter screen name
+  def twitter_screen_name_validation
+    unless twitter_screen_name.blank?
+      begin
+        if twitter_screen_name.match(/^@[a-zA-Z0-9]+/)
+          t = Twitter.user(twitter_screen_name)
+        else
+          errors.add(:twitter_screen_name, 'El usuario de Twitter no es válido')
+        end
+      rescue Exception => e
+        errors.add(:twitter_screen_name, 'El usuario de Twitter no existe')
+      end
     end
   end
 
