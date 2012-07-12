62b5e77a3ebe5430a59d602897c8c0a62a1f1163
Rodrigo Dominguez <rorra@rorra.com.ar>
2012-07-12 17:51:30 -0300

    Quick fix

--- app/helpers/advertiser/campaigns_helper.rb
+++ app/helpers/advertiser/campaigns_helper.rb
@@ -19,7 +19,7 @@ module Advertiser::CampaignsHelper
   end
 
   def statistics_tweet_cpms(tweet)
-    tweet_cost = tweet.fee_type = 'tweet_fee' ? tweet.tweet_fee : tweet.cpc_fee * tweet.clicks_count
+    tweet_cost = tweet.fee_type == 'tweet_fee' ? tweet.tweet_fee : tweet.cpc_fee * tweet.clicks_count
     sprintf("$ %.02f", tweet_cost * (1000 / tweet.influencer.audience.followers))
   end
 end
