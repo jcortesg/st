5211249b43a5ca8e2771a81e0b6338d379c60fce
Rodrigo Dominguez <rorra@rorra.com.ar>
2012-07-19 12:09:26 -0300

    New indexes for twitter users

---
+++ db/migrate/20120719150258_add_indexes_to_twitter_followers.rb
@@ -0,0 +1,9 @@
+class AddIndexesToTwitterFollowers < ActiveRecord::Migration
+  def change
+    add_index :twitter_followers, [:influencer_id, :twitter_user_id]
+    add_index :twitter_followers, [:influencer_id]
+    add_index :twitter_followers, [:twitter_user_id]
+
+    add_index :twitter_users, [:twitter_uid]
+  end
+end
