d2e6ad4fc608762163a6119bedc4415e439dbc1d
Rodrigo Dominguez <rorra@rorra.com.ar>
2012-07-23 11:41:49 -0300

    Sorted referrers

--- app/helpers/admin/referrers_helper.rb
+++ app/helpers/admin/referrers_helper.rb
@@ -1,2 +1,11 @@
 module Admin::ReferrersHelper
+  def sorted_referrer_list
+    referrers = User.where("role = 'influencer' or role = 'affiliate'").all
+    referrers.sort_by { |u| u.full_name }
+  end
+
+  def sorted_referral_list
+    referrals = User.where("role = 'influencer' or role = 'advertiser'").all
+    referrals.sort_by { |u| u.full_name }
+  end
 end
