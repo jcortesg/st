4518dadf243b0030cbc8682fdd0844772a7fb83e
Juan Manuel Amoros <juan.amoros@redmintsolutions.com>
2012-11-27 01:08:28 -0300

    Mercadopago Uno

---
+++ app/controllers/advertiser/payments_controller.rb
@@ -0,0 +1,55 @@
+# encoding: utf-8
+require 'mercadopago.rb'
+
+class Advertiser::PaymentsController < ApplicationController
+  before_filter :require_advertiser
+
+  # Show the list of transactions
+  def index
+    search_params = params[:search] || {}
+    search_params.reverse_merge!({"meta_sort" => "id.desc"})
+
+    if params[:search].nil?
+      search_params['updated_at_greater_than_or_equal_to'] = Date.today.beginning_of_month
+      search_params['updated_at_less_than_or_equal_to'] = Date.today
+    end
+
+    @search = Payment.where(user_id: current_user.id).search(search_params)
+    @payments = @search.page(params[:page])
+  end
+
+  # Shows a transaction
+  def show
+    @payment = Payment.find(params[:id])
+  end
+
+  def new
+    @payment = Payment.new
+  end
+
+  def create
+    @payment = Payment.new(params[:payment])
+    if @payment.save
+      flash[:notice] = "La orden de pago fue creada con éxito"
+      $mp = MercadoPago.new('8354375930808502', 'cBF2nCroJTRLIClUZAKXZiTVs4GX1Who')
+      $accessToken = $mp.get_access_token()
+      puts (accessToken)
+      preferenceData = Hash["items" => Array(Array["title"=>"testCreate", "quantity"=>1, "unit_price"=>10.2, "currency_id"=>"ARS"])]
+      preference = $mp.create_preference(preferenceData)
+      puts preference
+      redirect_to pay
+    else
+      flash.now[:error] = "Hubo un error al intentar crear la orden de pago"
+      render action: :new
+    end
+  end
+
+  def pay
+    @payment = Payment.find(params[:id])
+
+  end
+
+
+
+
+end
