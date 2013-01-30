1aa0cf93fbd546693570557bcf310dc4eef5a694
Rodrigo Dominguez <rorra@rorra.com.ar>
2013-01-30 17:41:27 -0300

    Deploy fixed

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,1 +1,4 @@
-
+set :rvm_ruby_string, '1.9.3'
+require "rvm/capistrano"
+set :rvm_type, :system
+require "bundler/capistrano"
--- config/deploy/argentina.rb
+++ config/deploy/argentina.rb
@@ -1,12 +1,3 @@
-# Add RVM's lib directory to the load path.
-#$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
-# Load RVM's capistrano plugin.
-require "rvm/capistrano"
-# Set it to the ruby + gemset of your app, e.g:
-set :rvm_ruby_string, '1.9.3'
-set :rvm_bin_path, "/usr/local/rvm/bin"
-set :rvm_type, :system
-
 set :application, "SocialTarget"
 set :repository, "git@bitbucket.org:sjasminoy/social-target.git"
 
@@ -23,13 +14,10 @@ set :deploy_via, :remote_cache
 set :deploy_to, "/u/apps/social-target-ar"
 set :branch, "master"
 
-set :asset_env, "RAILS_GROUPS=assets"
-
 namespace :setup do
   desc "Copy config files"
   task :copy_files, :roles => :app do
     run "cp #{release_path}/config/i18n-database/database-AR.yml #{release_path}/config/database.yml"
-    run "ln -s #{shared_path}/.rvmrc #{release_path}/.rvmrc"
   end
 
   desc "Copy config files for colombia"
@@ -39,24 +27,9 @@ namespace :setup do
     run "cp -r #{release_path}/config/i18n-files/application-AR.rb #{release_path}/config/application.rb"
   end
 
-  desc "Copy pictures files"
-  task :copy_pictures, :roles => :app do
-    run "cp -r #{previous_release}/public/bwn-image #{release_path}/public/"
-  end
-
-  desc 'Trust rvmrc file'
-  task :trust_rvmrc do
-    run "rvm rvmrc trust #{current_release}"
-  end
-
-  desc 'Bundle gems'
-  task :bundle_gems do
-    run "cd #{current_release} && #{try_sudo} bundle install --gemfile #{current_release}/Gemfile --path /var/www/social-target-ar/shared/bundle --without development test cucumber"
-  end
-
-  desc 'Precompile assets'
-  task :precompile_assets, :roles => :app do
-    run "cd #{release_path} && bundle exec #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile"
+  desc 'Link bwn images'
+  task :link_bwn_images do
+    run "ln -s #{shared_path}/bwn-image #{release_path}/public/bwn-image"
   end
 end
 
@@ -66,9 +39,6 @@ namespace :deploy do
   end
 end
 
-after "deploy:update_code", "setup:copy_files"
-after "deploy:update_code", "setup:copy_config_files"
-after "deploy:update_code", "setup:trust_rvmrc"
-after "deploy:update_code", "setup:copy_pictures"
-after "deploy:update_code", "setup:bundle_gems"
-after "deploy:update_code", "setup:precompile_assets"
+before "deploy:assets:precompile", "setup:copy_files"
+before "deploy:assets:precompile", "setup:copy_config_files"
+before "deploy:assets:precompile", "setup:link_bwn_images"
